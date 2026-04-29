96de6e7a51d6c1914de1c16ca52606b77474572d
satoh-y-0323 <shoma.papa.0323@gmail.com>
2026-04-29 19:26:51 +0900

    parallel-orchestra: task tdd-addition [auto-commit]

--- example/src/addition.py
+++ example/src/addition.py
@@ -3,4 +3,4 @@ from typing import Union
 
 def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     """Return the sum of a and b."""
-    raise NotImplementedError
+    return a + b
